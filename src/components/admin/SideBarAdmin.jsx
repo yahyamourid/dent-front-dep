86b763c05c81bacbb04437d21f2cd25c48802f19
Yahya MOURID <128039351+yahyamourid@users.noreply.github.com>
2023-12-30 14:34:33 +0100

    Update SideBarAdmin.jsx

--- src/components/admin/SideBarAdmin.jsx
+++ src/components/admin/SideBarAdmin.jsx
@@ -99,15 +99,7 @@ const SideBarAdmin = () => {
                 Dashboard
               </Link>
             </li>
-            <li>
-              <Link
-                className='flex items-center gap-x-3.5 py-2 px-2.5  text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-900 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'
-                to='/professor/student'
-              >
-               <FontAwesomeIcon icon={faUser} />
-                 Professors
-              </Link>
-            </li>
+           
           
             
 
